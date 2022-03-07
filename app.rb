e2e80566e7b9ad9ac9384e95a79d106e458d75e5
DimaPashchenko <kitaminchik@gmail.com>
2022-03-07 16:00:32 +0200

    Add Post entity

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, {adapter: "sqlite3", database: "blog.db"}
+
+class Post < ActiveRecord::Base
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
