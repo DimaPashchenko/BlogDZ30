dc409218bb710712f9f5ca1e987f099bf55a7162
DimaPashchenko <kitaminchik@gmail.com>
2022-03-08 12:56:12 +0200

    Add Comments migration

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ set :database, {adapter: "sqlite3", database: "blog.db"}
 class Post < ActiveRecord::Base
 end
 
+class Comment < ActiveRecord::Base
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
